c2530dac98c0c1025b90745cbac3496ff8610392
Ian Iversen <ianiversen1@gmail.com>
2023-01-11 20:18:04 -0500

    deleted first attempt for messing with file structure

--- first-tests/subtract.test.js
+++
@@ -1,5 +0,0 @@
-const subtract = require('./subract');
-
-test('properly subtracts two numbers', () => {
-    expect(subtract(1, 2)).toBe(-1);
-});
--- first-tests/sum.js
+++
@@ -1,9 +0,0 @@
-function sum(a, b) {
-    return a + b;
-}
-
-function helper() {
-    console.log('helper');
-}
-
-module.exports = sum;
